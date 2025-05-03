f4754e58c8043fad1c4bf5938b65e44391ba217b
Urvashi <satraurvashi99@gmail.com>
2025-05-03 23:07:34 +0530

    < On branch master

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -3,6 +3,7 @@ package com.ecommerce.project.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,7 @@ public class Category {
 
     @NotBlank
     @Column(name = "categoryName")
+    @Size(min = 5 , message = "Category name must contain atleast 5 characters")
     private String categoryName;
 
     public Category() {
