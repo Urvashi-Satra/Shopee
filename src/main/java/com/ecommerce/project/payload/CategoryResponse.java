31caf8ff17ea84a958e97b0914a159796094277f
Urvashi Satra <satraurvashi99@gmail.com>
2025-05-11 01:13:20 +0530

    App constant added

--- src/main/java/com/ecommerce/project/payload/CategoryResponse.java
+++ src/main/java/com/ecommerce/project/payload/CategoryResponse.java
@@ -10,13 +10,52 @@ import java.util.List;
 public class CategoryResponse {
 
     private List<CategoryDTO> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalPages;
+    private boolean lastPage;
 
-    public CategoryResponse(List<CategoryDTO> content) {
-        this.content = content;
+    public Integer getPageNumber() {
+        return pageNumber;
     }
 
-    public CategoryResponse(){
+    public void setPageNumber(Integer pageNumber) {
+        this.pageNumber = pageNumber;
     }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(Long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public boolean isLastPage() {
+        return lastPage;
+    }
+
+    public void setLastPage(boolean lastPage) {
+        this.lastPage = lastPage;
+    }
+
 
     public List<CategoryDTO> getContent() {
         return content;
@@ -24,5 +63,31 @@ public class CategoryResponse {
 
     public void setContent(List<CategoryDTO> content) {
         this.content = content;
+    }
+    public CategoryResponse(List<CategoryDTO> content) {
+        this.content = content;
+    }
+    public CategoryResponse(){
+    }
+
+    public CategoryResponse(List<CategoryDTO> content, Integer pageNumber, Integer pageSize, Long totalElements, Integer totalPages, boolean lastPage) {
+        this.content = content;
+        this.pageNumber = pageNumber;
+        this.pageSize = pageSize;
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+        this.lastPage = lastPage;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryResponse{" +
+                "content=" + content +
+                ", pageNumber=" + pageNumber +
+                ", pageSize=" + pageSize +
+                ", totalElements=" + totalElements +
+                ", totalPages=" + totalPages +
+                ", lastPage=" + lastPage +
+                '}';
     }
 }
